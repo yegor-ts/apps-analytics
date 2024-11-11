4156f3ccd520e6832a9278e38fd441b89482f64a
yegor-ts <69nonesons@gmail.com>
2024-11-12 00:50:22 +0200

    Setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(databaseConfig)],
   controllers: [],
   providers: [],
 })
